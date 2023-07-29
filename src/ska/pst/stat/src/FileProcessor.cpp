9a5c49178b0494f3fbbd7ba20c88a47d893e70b9
Willem van Straten <vanstraten.willem@gmail.com>
2023-07-29 21:06:17 +1200

    AT3-521 StatProcessor stubbed until at3-516-develop-statprocessor is merged

--- src/ska/pst/stat/src/FileProcessor.cpp
+++ src/ska/pst/stat/src/FileProcessor.cpp
@@ -37,24 +37,34 @@
 #include "ska/pst/stat/FileProcessor.h"
 
 ska::pst::stat::FileProcessor::FileProcessor(
-        const ska::pst::common::AsciiHeader& config,
+        const ska::pst::common::AsciiHeader& _config,
         const std::string& data_file_path,
         const std::string& weights_file_path)
-        : block_loader(new ska::pst::common::DataWeightsFileBlockLoader(data_file_path, weights_file_path)),
-          processor(new StatProcessor(config))
+        : processor(new StatProcessor(_config)),
+	  config(_config),
+	  block_loader(new ska::pst::common::DataWeightsFileBlockLoader(data_file_path, weights_file_path))
 {
   SPDLOG_DEBUG("ska::pst::stat::FileProcessor::FileProcessor");
 }
 
 ska::pst::stat::FileProcessor::~FileProcessor()
 {
-  SPDLOG_DEBUG("ska::pst::stat::FileProcessor::~FileProcessor()");
-
-  auto block = block_loader->next_block();
-  processor->process (block.data_block, block.data_size, block.weights_block, block.weights_size);
+  SPDLOG_DEBUG("ska::pst::stat::FileProcessor::~FileProcessor");
 }
 
 void ska::pst::stat::FileProcessor::process()
 {
   SPDLOG_DEBUG("ska::pst::stat::FileProcessor::process");
+
+  auto block = block_loader->next_block();
+  processor->process (block.data_block, block.data_size, block.weights_block, block.weights_size);
 }
+
+
+// stubs to fake compilation until at3-516-develop-statprocessor is merged
+
+#include "ska/pst/stat/StatPublisher.h"
+
+ska::pst::stat::StatProcessor::StatProcessor(ska::pst::common::AsciiHeader const&) {}
+ska::pst::stat::StatProcessor::~StatProcessor() {}
+void ska::pst::stat::StatProcessor::process(char*, unsigned long, char*, unsigned long) {}
